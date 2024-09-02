5e3b78fb05155e5457f4cb4334453da67b2669e5
red-001 <red-001@outlook.ie>
2024-09-02 01:10:17 +0100

    Add unittests for encryption

--- src/unittest/test_connection.cpp
+++ src/unittest/test_connection.cpp
@@ -180,8 +180,8 @@ void TestConnection::testConnectSendReceive()
 	infostream << "** Creating client Connection" << std::endl;
 	con::Connection client(512, 5.0f, false, &hand_client);
 
-	UASSERT(hand_server.count == 0);
-	UASSERT(hand_client.count == 0);
+	UASSERTEQ(s32, hand_server.count, 0);
+	UASSERTEQ(s32, hand_client.count, 0);
 
 	sleep_ms(50);
 
@@ -278,6 +278,7 @@ void TestConnection::testConnectSendReceive()
 
 		auto recvdata = pkt.oldForgePacket();
 
+		UASSERTEQ(size_t, sentdata.getSize(), recvdata.getSize());
 		UASSERT(memcmp(*sentdata, *recvdata, recvdata.getSize()) == 0);
 	}
 
@@ -345,6 +346,7 @@ void TestConnection::testConnectSendReceive()
 			infostream << "...";
 		infostream << std::endl;
 
+		UASSERTEQ(size_t, sentdata.getSize(), recvdata.getSize());
 		UASSERT(memcmp(*sentdata, *recvdata, recvdata.getSize()) == 0);
 		UASSERT(peer_id == PEER_ID_SERVER);
 	}
